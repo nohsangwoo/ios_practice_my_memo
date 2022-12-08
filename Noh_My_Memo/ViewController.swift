a4489801a015ca9f464f77379dce2a42cab55896
nohsangwoo <nsgr12@naver.com>
2022-12-08 20:21:21 +0900

    Initial Commit

---
+++ Noh_My_Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noh_My_Memo
+//
+//  Created by Noh Sangwoo on 2022/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
