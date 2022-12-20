0a88f6b81fe6b5cbb4cd4a4d84b14ea9cb8cb550
nohsangwoo <nsgr12@naver.com>
2022-12-20 22:52:07 +0900

    메모 목록에서 리렌더링시마다 최신화된 데이터를 렌더링 하기

--- Noh_My_Memo/MemoCell.swift
+++ Noh_My_Memo/MemoCell.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class MemoCell: UITableViewCell {
 
-    @IBOutlet weak var subject: UILabel!
+    @IBOutlet weak var subject: UILabel! // 메모 제목
     
-    @IBOutlet weak var contents: UILabel!
+    @IBOutlet weak var contents: UILabel! // 메모 내용
     
-    @IBOutlet weak var regdate: UILabel!
+    @IBOutlet weak var regdate: UILabel! // 등록 일자
     
-    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var img: UIImageView! // 이미지
 }
